2368bf47b8f11d8c0beeb4f21eb7795f2b850fea
Mika <erri.mika@gmail.com>
2017-06-24 14:20:27 +0300

    Configured screens.lua.

--- src/local/screens.lua
+++ src/local/screens.lua
@@ -7,9 +7,9 @@ return {
       width               = 1920,
       height              = 1080,
       screen_real_index   = 3,
-      screen_top_index    = 1,
+      screen_top_index    = 5,
       screen_right_index  = 2,
-      screen_bottom_index = 1,
+      screen_bottom_index = 5,
       screen_left_index   = 4
    },
    {
@@ -33,9 +33,9 @@ return {
       width               = 1920,
       height              = 1080,
       screen_real_index   = 2,
-      screen_top_index    = 3,
+      screen_top_index    = 6,
       screen_right_index  = 4,
-      screen_bottom_index = 3,
+      screen_bottom_index = 6,
       screen_left_index   = 2
    },
    {
@@ -58,14 +58,29 @@ return {
       name                = "VIRTUAL2",
       password            = "19801308qqq",
       port                = 5901,
-      start_x             = 3840,
+      start_x             = 0,
       start_y             = 1081,
       width               = 1920,
       height              = 1080,
       screen_real_index   = 5,
+      screen_top_index    = 1,
+      screen_right_index  = 6,
+      screen_bottom_index = 1,
+      screen_left_index   = 6
+   },
+   {
+      type                = "virtual",
+      name                = "VIRTUAL2",
+      password            = "19801308qqq",
+      port                = 5901,
+      start_x             = 3840,
+      start_y             = 1081,
+      width               = 1280,
+      height              = 1024,
+      screen_real_index   = 6,
       screen_top_index    = 3,
-      screen_right_index  = 4,
+      screen_right_index  = 5,
       screen_bottom_index = 3,
-      screen_left_index   = 2
+      screen_left_index   = 5
    }
 }
